45349ff3338f04420c45498d202f5cb530e4ac6a
G3 <tuxgal@Baymax.area51>
2024-05-30 00:04:26 -0700

    feat: Handle updating enabled/disabled integrations in update-latest-upstream.sh.

--- scripts/update-latest-upstream.sh
+++ scripts/update-latest-upstream.sh
@@ -6,6 +6,8 @@ script_parent_dir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 git_repo_dir="$(realpath "${script_parent_dir:?}/..")"
 
 ARGS_FILE="${git_repo_dir:?}/config/ARGS"
+ENABLED_INTGRATIONS_FILE="${git_repo_dir:?}/config/enabled-integrations.txt"
+DISABLED_INTGRATIONS_FILE="${git_repo_dir:?}/config/disabled-integrations.txt"
 
 git_repo_get_all_tags() {
     git_repo="${1:?}"
@@ -25,6 +27,13 @@ git_repo_latest_tag() {
     highest_sem_ver_tag=$(git_repo_get_all_tags ${git_repo:?} | sed -E s'#^v(.*)$#\1#g' | sed '/-/!{s/$/_/}' | sort --version-sort | sed 's/_$//'| tail -1)
     # Identify the correct tag for the semantic version of interest.
     git_repo_get_all_tags ${git_repo:?} | grep -E "${highest_sem_ver_tag//./\\.}$" | cut --delimiter='/' --fields=3
+}
+
+update_integration_files() {
+    ha_version="${1:?}"
+    hasspkgutil_version=$(get_config_arg "HASS_PKG_UTIL_VERSION")
+    base_image=$(get_config_arg "BASE_IMAGE_NAME"):$(get_config_arg "BASE_IMAGE_TAG")
+    docker run --rm -v ${ENABLED_INTGRATIONS_FILE:?}:/root/ei.txt -v ${DISABLED_INTGRATIONS_FILE:?}:/root/di.txt ${base_image:?} sh -c "homelab install-tuxdude-go-package TuxdudeHomeLab/hasspkgutil ${hasspkgutil_version:?} >/dev/null 2>&1 && hasspkgutil -mode-update -ha-version ${ha_version:?} -enabled-integrations /root/ei.txt -disabled-integrations /root/di.txt"
 }
 
 get_config_arg() {
@@ -48,8 +57,10 @@ latest_upstream_ver=$(git_repo_latest_tag ${repo_url:?})
 if [[ "${existing_upstream_ver:?}" == "${latest_upstream_ver:?}" ]]; then
     echo "Existing config is already up to date and pointing to the latest upstream ${pkg:?} version '${latest_upstream_ver:?}'"
 else
+    update_integration_files "${latest_upstream_ver:?}"
+
     echo "Updating ${pkg:?} ${config_ver_key:?} '${existing_upstream_ver:?}' -> '${latest_upstream_ver:?}'"
     set_config_arg "${config_ver_key:?}" "${latest_upstream_ver:?}"
-    git add ${ARGS_FILE:?}
+    git add ${ARGS_FILE:?} ${ENABLED_INTGRATIONS_FILE:?} ${DISABLED_INTGRATIONS_FILE:?}
     git commit -m "feat: Bump upstream ${pkg:?} version to ${latest_upstream_ver:?}."
 fi

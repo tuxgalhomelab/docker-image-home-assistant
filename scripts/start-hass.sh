80eb4a59b77ed33b64b6f35dc357ffd84110b136
G3 <tuxgal@Baymax.area51>
2024-06-16 18:16:18 -0700

    feat: Set umask to 0002 granting write permissions to the group.

--- scripts/start-hass.sh
+++ scripts/start-hass.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -E -e -o pipefail
 
+set_umask() {
+    # Configure umask to allow write permissions for the group by default
+    # in addition to the owner.
+    umask 0002
+}
+
 start_home_assistant () {
     echo "Starting Home Assistant ..."
     echo
@@ -9,4 +15,5 @@ start_home_assistant () {
     exec hass --config /config --log-file /dev/stdout --log-rotate-days 0
 }
 
+set_umask
 start_home_assistant
